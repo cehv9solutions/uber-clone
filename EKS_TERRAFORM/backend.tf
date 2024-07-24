8d3fb5198325a3e467309f75f2a9a7a9c087874c
cehv9solutions <cehv9solutions+1@gmail.com>
2024-07-25 00:04:58 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -3,13 +3,3 @@ terraform {
     path = "./terraform.tfstate"
   }
 }
-
-
-/* terraform {
-  backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
-*/
